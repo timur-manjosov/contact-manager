7a7e4432edb4c7c11847319c92f6238326bdbf5e
timur-manjosov <timurmanjosov.dev@proton.me>
2026-06-09 08:31:34 +0200

    Save and load contacts from JSON File

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
+import json
+
+try:
+    with open("contacts.json", "r") as file:
+        contacts = json.load(file)
+        print("Contact list was loaded!")
+
+except FileNotFoundError:
+    print("Right now, there are no contacts. Please go forward with adding contacts!")
+    contacts = {}
+ 
+
 while True:
     decison = input("1) List contacts\n 2) Add a contact\n 3) Leave!\n Chosse an option from 1 to 3: ")
 
     if decison == "1":
-        print("Will come 1")
-        break
+        if contacts: 
+            for key, value in contacts.items():
+                print(f"{key} -> {value}")
+        else:
+            print("There are currently no entries in contact. Please go forward by adding some contacts!")
+
     elif decison == "2":
-        print("Will come 2")
-        break
+        name = input("Please continue with a name: ")
+        number = input("Now give the name a telephon number: ")
+        contacts[name] = number
     elif decison == "3":
-        print("Bye:)")
+
+        with open("contacts.json", "w") as datei:
+            json.dump(contacts, datei)
+
+        print("Bye")
         break
 
     else:
