bdc4edd22907fd5b7ea2b8780c727c124e38721a
timur-manjosov <timurmanjosov.dev@proton.me>
2026-06-10 06:17:14 +0200

    Refactor to object-oriented design with AddressBook class

--- main.py
+++ main.py
@@ -1,60 +1,70 @@
 import json
 
-def show_contacts(contacts):
-    if contacts: 
-        for key, value in contacts.items():
-            print(f"{key} -> {value}")
-    else:
-        print("There are currently no entries in contact. Please go forward by adding some contacts!")
+class AddressBook:
+    def __init__(self):
+        self.contacts = {}
+        self.load()
 
-def search_contact(contacts):
-    name = input("Please continue with a name: ")
-    if name in contacts:
-        number = contacts[name]
-        print(f"The contact {name} does have the phone number: {number}")
-    else:
-        print("The user you want to find does not exist.")
+    def load(self):
+        try:
+            with open("contacts.json", "r") as file:
+                self.contacts = json.load(file)
+            print("Contact list was loaded!")
 
-def add_contact(contacts):
-    name = input("Please continue with a name: ")
-    number = input("Now give the name a telephon number: ")
-    contacts[name] = number
+        except FileNotFoundError:
+            print("Right now, there are no contacts. Please go forward with adding contacts!")
+            self.contacts = {}
 
-def delete_contact(contacts):
-    name = input("Please continue with a name: ")
-    if name in contacts:
-        del contacts[name]
-    else:
-        print("This contact does not exist!")
+    def show_contacts(self):
+        if self.contacts:
+            for key, value in self.contacts.items():
+                print(f"{key} -> {value}")
+        else:
+            print("There are currently no entries in contact. Please go forward by adding some contacts!")
 
-def save_contacts(contacts):
-    with open("contacts.json", "w") as datei:
-        json.dump(contacts, datei)
+    def search_contact(self):
+        name = input("Please continue with a name: ")
+        if name in self.contacts:
+            number = self.contacts[name]
+            print(f"The contact {name} does have the phone number: {number}")
+        else:
+            print("The user you want to find does not exist.")
 
-    print("The contact list was saved!")
+    def add_contact(self):
+        name = input("Please continue with a name: ")
+        number = input("Now give the name a telephon number: ")
+        self.contacts[name] = number
 
-try:
-    with open("contacts.json", "r") as file:
-        contacts = json.load(file)
-        print("Contact list was loaded!")
+    def delete_contact(self):
+        name = input("Please continue with a name: ")
+        if name in self.contacts:
+            del self.contacts[name]
+        else:
+            print("This contact does not exist!")
 
-except FileNotFoundError:
-    print("Right now, there are no contacts. Please go forward with adding contacts!")
-    contacts = {}
+    def save_contacts(self):
+        with open("contacts.json", "w") as datei:
+            json.dump(self.contacts, datei)
+
+        print("The contact list was saved!")
+
+
+book = AddressBook()
+
 
 while True:
     decison = input("1) List contacts\n 2) Search a contact\n 3) Add a contact\n 4) Delete a contact\n 5) Leave!\n Choose an option from 1 to 5: ")
 
     if decison == "1":
-        show_contacts(contacts)
+        book.show_contacts()
     elif decison == "2":
-        search_contact(contacts)
+        book.search_contact()
     elif decison == "3":
-        add_contact(contacts)
-        save_contacts(contacts)
+        book.add_contact()
+        book.save_contacts()
     elif decison == "4":
-        delete_contact(contacts)
-        save_contacts(contacts)
+        book.delete_contact()
+        book.save_contacts()
     elif decison == "5":
         print("Bye!")
         break
