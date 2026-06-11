f19311fec22194e5715a808230a0851abfad03dd
timur-manjosov <timurmanjosov.dev@proton.me>
2026-06-11 05:59:19 +0200

    Add Contact class with email field and JSON serialization

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import json
 
+
+class Contact:
+    def __init__(self, name, number, email):
+        self.name = name
+        self.number = number
+        self.email = email
+
+    def show(self):
+        print(f"Name: {self.name} | Number: {self.number} | Email: {self.email}")
+
+    def to_dict(self):
+        return {"name": self.name, "number": self.number, "email": self.email}
+
 class AddressBook:
     def __init__(self):
         self.contacts = {}
@@ -8,32 +21,34 @@ class AddressBook:
     def load(self):
         try:
             with open("contacts.json", "r") as file:
-                self.contacts = json.load(file)
+                raw = json.load(file)        # rohe Mappen aus der Datei
+            self.contacts = {}               # leeres Regal, das wir füllen
+            for key, value in raw.items():
+                self.contacts[key] = Contact(value["name"], value["number"], value["email"])   # <- DEINE Kernzeile
             print("Contact list was loaded!")
-
         except FileNotFoundError:
             print("Right now, there are no contacts. Please go forward with adding contacts!")
             self.contacts = {}
 
     def show_contacts(self):
         if self.contacts:
-            for key, value in self.contacts.items():
-                print(f"{key} -> {value}")
+            for contact in self.contacts.values():
+                contact.show()
         else:
             print("There are currently no entries in contact. Please go forward by adding some contacts!")
 
     def search_contact(self):
         name = input("Please continue with a name: ")
         if name in self.contacts:
-            number = self.contacts[name]
-            print(f"The contact {name} does have the phone number: {number}")
+            self.contacts[name].show()
         else:
             print("The user you want to find does not exist.")
 
     def add_contact(self):
         name = input("Please continue with a name: ")
         number = input("Now give the name a telephon number: ")
-        self.contacts[name] = number
+        email = input("Close this process by giving a email: ")
+        self.contacts[name] = Contact(name, number, email)
 
     def delete_contact(self):
         name = input("Please continue with a name: ")
@@ -43,8 +58,11 @@ class AddressBook:
             print("This contact does not exist!")
 
     def save_contacts(self):
+        data = {}
+        for key, value in self.contacts.items():
+            data[key] = value.to_dict()
         with open("contacts.json", "w") as datei:
-            json.dump(self.contacts, datei)
+            json.dump(data, datei)
 
         print("The contact list was saved!")
 
